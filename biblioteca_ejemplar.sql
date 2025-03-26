f543677b4f12eaa033b6c92731256c377292aae5
Juanes <tuemail@ejemplo.com>
2025-03-26 00:43:06 -0500

    Esta base de datos MySQL contiene información sobre los ejemplares físicos de los libros en la biblioteca. La tabla ejemplar permite registrar la ubicación específica de cada ejemplar y su relación con un libro en particular. Cada ejemplar tiene un ID único, una localización que indica dónde se encuentra dentro de la biblioteca y una clave foránea libro_id, que lo asocia con un libro en la tabla libro. Esta estructura es esencial para la gestión de inventarios, permitiendo localizar fácilmente un ejemplar y administrar su disponibilidad para préstamos o consulta.

---
+++ biblioteca_ejemplar.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ejemplar`
+--
+
+DROP TABLE IF EXISTS `ejemplar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ejemplar` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `localizacion` varchar(30) NOT NULL,
+  `libro_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `libro_id` (`libro_id`),
+  CONSTRAINT `ejemplar_ibfk_1` FOREIGN KEY (`libro_id`) REFERENCES `libro` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ejemplar`
+--
+
+LOCK TABLES `ejemplar` WRITE;
+/*!40000 ALTER TABLE `ejemplar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ejemplar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-25 23:57:33
